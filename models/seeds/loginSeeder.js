b2b7a3ab758e0e7b078a3b93f8b26ccf422962e5
r05323045 <r05323045@ntu.edu.tw>
2020-09-27 12:08:18 +0800

    feat: add login seeder

---
+++ models/seeds/loginSeeder.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const Login = require('../login')
+mongoose.connect('mongodb://localhost/login-feature', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  users.forEach(user => {
+    Login.create(user)
+  })
+  console.log('done')
+})
